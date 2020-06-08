84d3e6ad24a9992769aac0ff67490df178b5b373
modiavi <modiavi@users.noreply.github.com>
2020-06-08 09:26:37 -0400

    Create autofill.js

---
+++ autofill.js
@@ -0,0 +1,37 @@
+//Step 1
+var pagebutton = document.getElementById('NextButton');
+pagebutton.click();
+setTimeout(step2, 2000);
+//Step 2
+function step2(){
+    var myname = document.getElementById('QR~QID2');
+    myname.value = 'Avi Modi';
+    var pagebutton = document.getElementById('NextButton');
+    pagebutton.click();
+    setTimeout(step3, 2000);
+}
+
+
+//Step 3
+function step3(){
+    var myname = document.getElementById('QR~QID3');
+    myname.value = 'Michael Brattman';
+    var pagebutton = document.getElementById('NextButton');
+    pagebutton.click();
+    setTimeout(step4, 2000);  
+}
+
+//Step 4
+function step4(){
+    document.getElementById('QR~QID4~7').checked = true;
+    var pagebutton = document.getElementById('NextButton');
+    pagebutton.click();
+    setTimeout(step5, 2000);
+}
+
+//Step 5
+function step5() {
+  document.getElementById('QR~QID5~2').checked = true;
+  var pagebutton = document.getElementById('NextButton');
+  pagebutton.click();  
+}
